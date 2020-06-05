eebfbc592fcf70a49d3b535583aa35a244af547e
曹雅男 <cyn_rich@163.com>
2020-06-05 10:44:02 +0800

    solve level3

--- tests/test_solutions.py
+++ tests/test_solutions.py
@@ -4,6 +4,7 @@
     Create by Cao Ya'nan on 2020-06-04
 """
 import string
+from collections import Counter
 from string import ascii_letters
 import re, urllib.request, webbrowser as wb
 
@@ -27,7 +28,25 @@ def test_1():
 def test_2():
     with urllib.request.urlopen('http://www.pythonchallenge.com/pc/def/ocr.html') as f:
         result = re.findall('<!--(.*?)-->', f.read().decode('utf8'), re.S)
-        print(result)
-        print(''.join([c for c in result[1] if c in ascii_letters]))
+        # print(result)
+        # print(''.join([c for c in result[1] if c in ascii_letters]))
 
-    # wb.open('http://www.pythonchallenge.com/pc/def/%s.html'%.join())
+        c = Counter(result[1])
+        asc_counter = [x for x in c.most_common() if x[0] in string.ascii_letters]
+        min_num = min([i[1] for i in asc_counter])
+        min_asc = [x[0] for x in asc_counter if x[1] == min_num]
+        print(''.join(min_asc))
+
+
+    # wb.open('http://www.pythonchallenge.com/pc/def/equality.html')
+
+
+def test_3():
+    with urllib.request.urlopen('http://www.pythonchallenge.com/pc/def/equality.html') as f:
+        text = re.findall('<!--(.*?)-->', f.read().decode('utf8'), re.S)
+        # text[0] = text[0].replace('\n', '')
+        # re_pattern = ''
+        # result = re.findall('([a-z].{80}?)?([A-Z].{80}?)([A-Z].{80}?)([A-Z].{77}?)([A-Z]{3}([a-z])[A-Z]{3}?)(.{77}[A-Z]{1}?)(.{80}[A-Z]{1}?)(.{80}[A-Z]{1}?)(.{79}[a-z]{1}?)?', text[0], re.S)
+        result = re.findall('[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]', text[0])
+        print(result)
+        print(''.join([i[0] for i in result]))
